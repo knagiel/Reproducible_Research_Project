972e886dc3087372bfa671dbba36e1e3a41f1012
limetonik <98454085+limetonik@users.noreply.github.com>
2023-06-18 22:10:00 +0200

    Update logit_and_probit.R to include Logit model calculation

--- logit_and_probit.R
+++ logit_and_probit.R
@@ -21,3 +21,27 @@ print(summary(data))
 # Correlation matrix
 cat("\nCorrelation matrix:\n")
 print(cor(data))
+
+# --- MODEL TRAINING --- LOGIT
+
+# Define features and target variables
+features <- c('Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age')
+x <- data[features]
+y <- data$Outcome
+
+# Split the dataset into training and testing sets
+# Setting seed for reproducibility
+set.seed(0)
+
+# Splitting the data
+splitIndex <- createDataPartition(data$Outcome, p = .90, list = FALSE, times = 1)
+train <- data[splitIndex,]
+test  <- data[-splitIndex,]
+
+# Fitting the logistic regression model
+logit <- glm(Outcome ~ ., data = train, family = binomial())
+
+# Display the model summary
+cat("\nLogistic Regression Model Summary:\n")
+print(summary(logit))
+
