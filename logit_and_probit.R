c04594d03fb5cc4e2198f462366e7eeedba59c92
knagiel <100476208+knagiel@users.noreply.github.com>
2023-06-18 22:45:46 +0200

    updates to script

--- logit_and_probit.R
+++ logit_and_probit.R
@@ -101,6 +101,38 @@ new_data <- data.frame(Pregnancies = 5, Glucose = 0, BloodPressure = 33.7,
                        SkinThickness = 50, Insulin = 150, BMI = 74, 
                        DiabetesPedigreeFunction = 0.5, Age = 53)
 
+# Creating a confusion matrix plot
+
+# create a function to draw the confusion matrix
+draw_confusion_matrix <- function(cm) {
+  layout(matrix(c(1,1,2)))
+  par(mar=c(2,2,2,2))
+  plot(c(100, 345), c(300, 450), type = "n", xlab="", ylab="", xaxt='n', yaxt='n')
+  title('CONFUSION MATRIX', cex.main=2)
+  
+  # create the matrix
+  rect(150, 430, 240, 370, col='#3F97D0')
+  text(195, 435, '0', cex=1.2)
+  rect(250, 430, 340, 370, col='#F7AD50')
+  text(295, 435, '1', cex=1.2)
+  text(125, 370, 'Predicted', cex=1.3, srt=90, font=2)
+  text(245, 450, 'Actual', cex=1.3, font=2)
+  rect(150, 305, 240, 365, col='#F7AD50')
+  rect(250, 305, 340, 365, col='#3F97D0')
+  text(140, 400, '0', cex=1.2, srt=90)
+  text(140, 335, '1', cex=1.2, srt=90)
+  
+  # add in the cm results
+  res <- as.numeric(cm)
+  text(195, 400, res[1], cex=1.6, font=2, col='white')
+  text(195, 335, res[2], cex=1.6, font=2, col='white')
+  text(295, 400, res[3], cex=1.6, font=2, col='white')
+  text(295, 335, res[4], cex=1.6, font=2, col='white')
+}
+
+# call the function with your confusion matrix
+draw_confusion_matrix(confusion_matrix)
+
 # Load the saved model
 loaded_model <- readRDS("logistic_reg.rds")
 
@@ -159,35 +191,5 @@ ourmodelprediction <- ifelse(ourmodelprediction > 0.5, 1, 0)
 cat("\nPrediction on new data:\n")
 print(ourmodelprediction)
 
-# Creating a confusion matrix plot
 
-# create a function to draw the confusion matrix
-draw_confusion_matrix <- function(cm) {
-  layout(matrix(c(1,1,2)))
-  par(mar=c(2,2,2,2))
-  plot(c(100, 345), c(300, 450), type = "n", xlab="", ylab="", xaxt='n', yaxt='n')
-  title('CONFUSION MATRIX', cex.main=2)
-  
-  # create the matrix
-  rect(150, 430, 240, 370, col='#3F97D0')
-  text(195, 435, '0', cex=1.2)
-  rect(250, 430, 340, 370, col='#F7AD50')
-  text(295, 435, '1', cex=1.2)
-  text(125, 370, 'Predicted', cex=1.3, srt=90, font=2)
-  text(245, 450, 'Actual', cex=1.3, font=2)
-  rect(150, 305, 240, 365, col='#F7AD50')
-  rect(250, 305, 340, 365, col='#3F97D0')
-  text(140, 400, '0', cex=1.2, srt=90)
-  text(140, 335, '1', cex=1.2, srt=90)
-  
-  # add in the cm results
-  res <- as.numeric(cm)
-  text(195, 400, res[1], cex=1.6, font=2, col='white')
-  text(195, 335, res[2], cex=1.6, font=2, col='white')
-  text(295, 400, res[3], cex=1.6, font=2, col='white')
-  text(295, 335, res[4], cex=1.6, font=2, col='white')
-}
-
-# call the function with your confusion matrix
-draw_confusion_matrix(confusion_matrix)
 
