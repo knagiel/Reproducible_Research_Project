55838bfa1c43fcb693a6ea2d4b469e27a6c52511
knagiel <100476208+knagiel@users.noreply.github.com>
2023-06-18 22:02:09 +0200

    adding data preparation script

--- logit_and_probit.R
+++ logit_and_probit.R
@@ -1,1 +1,23 @@
+# Load necessary libraries
+library(tidyverse)
+library(caret)
+library(glmnet)
+library(gplots)
 
+# --- DATA PREPARATION ---
+
+# Read the CSV file
+data <- read_csv("diabetes2.csv")
+
+# Display the structure of the dataframe
+str(data)
+
+# Check for missing values and provide a summary
+cat("Missing values per column:\n")
+print(colSums(is.na(data)))
+cat("\nData Summary:\n")
+print(summary(data))
+
+# Correlation matrix
+cat("\nCorrelation matrix:\n")
+print(cor(data))
